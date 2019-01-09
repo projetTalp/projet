98a8f67f1ae4ed3299dea90fd9f26f0b8610b22e
Albalyss <34515288+Albalyss@users.noreply.github.com>
2019-01-09 18:06:37 +0100

    add test eval

--- index.py
+++ index.py
@@ -9,12 +9,12 @@ html = """<!DOCTYPE html>
     <title> Bienvenue dans notre moteur de recherche</title>    
 </head>
 <body>
-	<div class="input-group mb-3">
-  		<input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
-  		<div class="input-group-append">
-    			<button class="btn btn-outline-secondary" type="button" id="button-addon2">Chercher</button>
-  		</div>
-	</div>
+    <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
+        <div class="input-group-append">
+            <button class="btn btn-outline-secondary" type="button" id="button-addon2">Chercher</button>
+        </div>
+    </div>
 </body>
 </html>
 """
