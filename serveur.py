6bb6c994b997c20c82e1b426a45dcf1b96c981e2
Amininthehood <abdellaoui.ahmed.amine@gmail.com>
2019-02-10 18:42:57 +0100

    derniere version W2V

--- serveur.py
+++ serveur.py
@@ -4,6 +4,8 @@ import time
 import getdf
 import trt_doc as td
 
+import word2Vec as wv
+
 @route('/index')
 def index():
 	return template("view/form_search.html")
@@ -21,6 +23,15 @@ def search():
 @route('/doc/<num>')	
 def getDoc(num):
 	return template("view/header.html") + "<p>" + td.getDoc("./data/database.json", int(num)) + "</p>" + template("view/footer.html")
+
+@post('/searchW2V')
+def searchW2V():
+	req = request.forms.get('request')
+	h = time.time()
+	rez = wv.get_results(req)
+	h = time.time() - h
+	return template("view/header.html") + template("view/form_result.html", query=req, time=h) + wv.showResult(rez) + template("view/footer.html")
+
 
 
 run(host='localhost', port=8080)
