83c96fde17d9b3520912b18eec3a32c86c601dba
Aminthehood <abdellaoui.ahmed.amine@gmail.com>
2019-02-10 13:48:05 +0100

    Update serveur.py

--- serveur.py
+++ serveur.py
@@ -3,7 +3,7 @@ from bottle import post, request
 import time
 import getdf
 import trt_doc as td
-
+import word2Vec as wv
 
 @route('/index')
 def index():
@@ -23,6 +23,13 @@ def search():
 def getDoc(num):
 	return template("view/header.html") + "<p>" + td.getDoc("./data/database.json", int(num)) + "</p>" + template("view/footer.html")
 
+@post('/searchW2V')
+def searchW2V():
+	req = request.forms.get('request')
+	h = time.time()
+	rez = wv.get_results(req)
+	h = time.time() - h
+	return template("view/header.html") + template("view/form_result.html", query=req, time=h) + wv.showResult(rez) + template("view/footer.html")
 
 run(host='localhost', port=8080)
 
