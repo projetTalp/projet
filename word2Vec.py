f4858883d159563065e5e9afc63d08bbd8eef8cc
Aminthehood <abdellaoui.ahmed.amine@gmail.com>
2019-02-26 15:32:10 +0100

    Update word2Vec.py

--- word2Vec.py
+++ word2Vec.py
@@ -3,7 +3,7 @@ import trt_doc as td
 
 import numpy as np
 
-
+# function that load our google pre-trained model, we had only used it once to load our model
 def getPreTrainedModel():
     # Load Google's pre-trained Word2Vec model.
     model = gensim.models.KeyedVectors.load_word2vec_format('./word2vec/GoogleNews-vectors-negative300-SLIM.bin', binary=True)
@@ -14,7 +14,9 @@ def getPreTrainedModel():
 
     return model
 
-
+# function that calculates the average vector for a string
+# calculates an average vector as the addition of all string's vectors divided by the number of vectors in that string
+# @parameters : words : string ; model : our word2vec model ; num_features : dimension of a vector ; index2word_set : index of our vocabulary 
 def mean_vector(words, model, num_features, index2word_set):
     # function to average all words vectors in a given paragraph
     featureVec = np.zeros((num_features,), dtype="float32")
@@ -29,44 +31,44 @@ def mean_vector(words, model, num_features, index2word_set):
         featureVec = np.divide(featureVec, nwords)
     return featureVec
 
-
+#function that calculates once and for all, the average vector for all our documents in the database
 def Word2vec_avg_doc(filename):
     dict = []
     t = td.load_json(filename)
     for i in range(0, len(t)):
-        t[i] = "".join(j for j in t[i] if not j.isdigit())
-        t[i] = td.cleanup(t[i])
+        t[i] = "".join(j for j in t[i] if not j.isdigit()) # In addition to tokenisation we omit all digits
+        t[i] = td.cleanup(t[i]) # preprocess every word in every document in our data base
         doc_avg_vector = mean_vector(t[i].split(), model=model, num_features=300, index2word_set=index2word_set)
         dict.append(doc_avg_vector)
-    np.save('word2vec/doc_vector.npy', dict)
+    np.save('word2vec/doc_vector.npy', dict) # we save our document's vectors once and for all to save time in calculus
 
-
+#function that calculates for every query its average vector 
 def Word2vec_avg_query(query):
     query_avg_vector = mean_vector(query.split(), model=model, num_features=300, index2word_set=index2word_set)
     return query_avg_vector
 
-
+# function that calculates cosine similarity between 2 vectors
 def cosine(v1, v2):
     v1 = np.array(v1)
     v2 = np.array(v2)
-    return np.dot(v1, v2) / (np.sqrt(np.sum(v1**2)) * np.sqrt(np.sum(v2**2)))
+    return np.dot(v1, v2) / (np.sqrt(np.sum(v1**2)) * np.sqrt(np.sum(v2**2))) # mathematical formula
 
-
+#function that calculates similarity between every document's vector in our data base and our query
 def similarity(query):
     results = {}
-    doc_vector = np.load('word2vec/doc_vector.npy')
-    query_vector = Word2vec_avg_query(query)
+    doc_vector = np.load('word2vec/doc_vector.npy') # We load our document's vectors
+    query_vector = Word2vec_avg_query(query) # Calculate our query vector
     for i in range(1, len(doc_vector)):
-        results[i] = cosine(doc_vector[i], query_vector)
-    return results
+        results[i] = cosine(doc_vector[i], query_vector) # calculating similarity between every document's vector in our data base and our query
+    return results # results are saved in a dictionary
 
-
+# function that sorts -and returns- our similarity's results saved in a dictionnary in decreasing order  
 def get_results(query):
     a = similarity(query)
     s = sorted(a.items(), key=lambda t: t[1], reverse=True)
     return s
 
-
+# Function that shows our results in our bottle server
 def showResult(sortedDicoOfSimi):
     html = "<div class='result'><h3>Listes des resultats</h3>"
     for doc in sortedDicoOfSimi:
@@ -74,7 +76,8 @@ def showResult(sortedDicoOfSimi):
     html += "</div>"
     return html
 
-
+# We first load our saved model 
 model = gensim.models.KeyedVectors.load('word2vec/google_word2vec')
+# Indexing its vocabulary's words
 index2word_set = set(model.wv.index2word)
 
